8326780be729c1dff80ec744dad0c7a695e47b2a
rajeshdumpeti <dumpetirajesh123@gmail.com>
2025-10-14 16:39:01 -0400

    This commit will add document upload, get and delete for the documents api

--- board-api/app/db/init_models.py
+++ board-api/app/db/init_models.py
@@ -7,6 +7,7 @@ from app.auth.models import User
 from app.models.project import Project
 from app.models.column import BoardColumn
 from app.models.task import Task
+from app.models.document import Document
 
 def init_db():
     """Create all database tables."""
